ce117a6af2478e57b4ddb6ea369147bb060306da
Vibhu Jawa <jawa@Vibhus-MacBook-Pro.local>
2018-04-07 15:34:29 -0400

    added dir

--- mimic3models/split_train_val.py
+++ mimic3models/split_train_val.py
@@ -12,7 +12,7 @@ assert args.task in ['decompensation', 'in-hospital-mortality', 'length-of-stay'
                      'phenotyping', 'multitask']
 
 val_patients = set()
-with open(data_dir+"mimic3models/valset.csv", "r") as valset_file:
+with open("mimic3models/valset.csv", "r") as valset_file:
     for line in valset_file:
         x, y = line.split(',')
         if int(y) == 1:
@@ -28,12 +28,12 @@ val_lines = [x for x in lines if x[:x.find("_")] in val_patients]
 assert len(train_lines) + len(val_lines) == len(lines)
 
 
-with open(data_dir+"%s/train_listfile.csv" % args.task, "w") as train_listfile:
+with open("%s/train_listfile.csv" % args.task, "w") as train_listfile:
     train_listfile.write(header)
     for line in train_lines:
         train_listfile.write(line)
 
-with open(data_dir+"%s/val_listfile.csv" % args.task, "w") as val_listfile:
+with open("%s/val_listfile.csv" % args.task, "w") as val_listfile:
     val_listfile.write(header)
     for line in val_lines:
         val_listfile.write(line)
